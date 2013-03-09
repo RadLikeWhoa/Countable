2021f47fb07f9db498e8511af2a7373ad4e7cb1d
Sacha Schmid <schmid.sacha@gmail.com>
2013-03-09 16:11:17 +0100

    Initial commit
    Add all the files to the gh-pages branch.

--- CONTRIBUTING.md
+++
@@ -1,4 +0,0 @@
-# Contributing to Countable
-
-* When submitting a pull request or an issue, be as precise as possible in wording the changes you've made or the problems you've encountered.
-* If applicable, provide a live example on [jsFiddle](http://jsfiddle.net) or [CodePen](http://codepen.io).
---
+++ config.rb
@@ -0,0 +1,26 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
