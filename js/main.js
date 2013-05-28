32bb453d81dee54615bee00a004defa3e9864aa6
Sacha Schmid <schmid.sacha@gmail.com>
2013-05-28 17:31:19 +0200

    Rewrite JS code

--- js/main.js
+++ js/main.js
@@ -1,101 +1,187 @@
 (function (document) {
-  var scr = document.scripts[0],
-      hasTextContent = 'textContent' in document.body,
-      area = document.getElementById('countableArea'),
-      githubButton = document.getElementById('github-button'),
-      fields = {
-        paragraphs: document.getElementById('result__paragraphs'),
-        words: document.getElementById('result__words'),
-        characters: document.getElementById('result__characters'),
-        all: document.getElementById('result__all')
-      },
-      _gaq = [['_setAccount', 'UA-39380123-1'], ['_trackPageview']]
+  /**
+   * As `textContent` is not supported in all browsers we need to set it
+   * differently depending on the browser's capabilities. This is mainly
+   * needed to support IE < 9.
+   *
+   * @see <http://goo.gl/EIhUJ>
+   */
 
-  // Adjusted bounce rate (http://drawingablank.me/blog/fix-your-bounce-rate.html)
+  function text (element, content) {
+    if('textContent' in document.body) {
+      element.textContent = content
+    } else {
+      element.innerText = content
+    }
+  }
+
+  /**
+   * IE uses a different way to bind event listeners to an element so we need
+   * to set that based on the browser's capabilities.
+   *
+   * @see <http://goo.gl/B3bGc>
+   */
+
+  function event (element, event, callback) {
+    if (window.addEventListener) {
+      element.addEventListener(event, callback)
+    } else if (window.attachEvent) {
+      element.attachEvent(event, callback)
+    }
+  }
+
+  /**
+   * Helper scripts are inserted asynchronously into the page.
+   */
+
+  function async (url) {
+    var g = document.createElement('script'),
+        s = document.scripts[0]
+
+    g.src = url
+    s.parentNode.insertBefore(g, s)
+  }
+
+  /**
+   * In order to adjust the bounce rate on Google Analytics, we can safely
+   * assume that if a visitor stays on the site for 15 seconds, he shouldn't
+   * be counted as a bounce visitor.
+   *
+   * @see <http://goo.gl/nyGIK>
+   */
 
   setTimeout(function () {
     _gaq.push(['_trackEvent', 'Site', 'read'])
   }, 15000);
 
-  // Set up Countable on the demo textarea
+  /**
+   * We set up Countable on the demo `textarea` and make it write the counted
+   * results in the relevant result fields.
+   */
 
-  Countable.live(area, function (counter) {
+  var fields = {
+    paragraphs: document.getElementById('result__paragraphs'),
+    words: document.getElementById('result__words'),
+    characters: document.getElementById('result__characters'),
+    all: document.getElementById('result__all')
+  }
+
+  Countable.live(document.getElementById('countableArea'), function (counter) {
     for (var field in fields) text(fields[field], counter[field])
   })
 
-  // Helper function to set an elements text in different browsers
+  /**
+   * We get the current watch count of Countable through the GitHub API and
+   * display it on the button then.
+   */
 
-  function text (element, content) {
-    hasTextContent ? element.textContent = content : element.innerText = content
-  }
+  async('https://api.github.com/repos/RadLikeWhoa/Countable?callback=watchCount')
 
-  // Get the current watcher count
+  window.watchCount = function (data) {
+    if (!data) return
 
-  window.gitHubWatchers = function (data) {
     var counter = document.getElementById('github-watch-count'),
         watchers = data.data.watchers
 
-    if (!counter || !watchers) return
+    if (!counter || !watchers) return
 
     text(counter, watchers)
     counter.parentNode.style.display = 'inline-block'
     counter.parentNode.title = 'Countable has ' + watchers + ' stargazers on GitHub'
 
-    window.gitHubWatchers = undefined
+    try {
+      delete window.watchCount
+    } catch (e) {
+      window.watchCount = undefined
+    }
   }
 
-  var g = document.createElement('script')
+  /**
+   * We load the Google Analytics snippet asynchronously and track every click
+   * on the 'Download on GitHub' button.
+   */
 
-  g.src = 'https://api.github.com/repos/RadLikeWhoa/Countable?callback=gitHubWatchers'
-  scr.parentNode.insertBefore(g, scr)
+  var _gaq = [['_setAccount', 'UA-39380123-1'], ['_trackPageview']]
 
-  // Google Analytics
+  async('http://www.google-analytics.com/ga.js')
 
-  ;(function () {
-    var g = document.createElement('script')
+  event(document.getElementById('github-button'), 'click', function () {
+    _gaq.push(['_trackEvent', 'Site', 'Downloads'])
+  })
 
-    g.src = '//www.google-analytics.com/ga.js'
-    scr.parentNode.insertBefore(g, scr)
-  }())
+  /**
+   * We make use of Twitter's widgets platform to use Web Intents and to be able
+   * to track Twitter actions (e.g. tweet, follow.)
+   *
+   * @see <http://goo.gl/gHefO>
+   * @see <http://goo.gl/4NyWj>
+   */
 
-  // Twitter
+  async('https://platform.twitter.com/widgets.js')
 
   window.twttr = (function () {
-    var js = document.createElement('script'),
-        t
-
-    js.src = 'https://platform.twitter.com/widgets.js'
-    scr.parentNode.insertBefore(js, scr)
+    var t
     return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } })
   }())
 
-  // Track twitter intent events on Google Analytics
-
-  function track (event) {
-    _gaq.push(['_trackEvent', 'Twitter', event.type])
-  }
-
   twttr.ready(function () {
+    var track = function (event) {
+      _gaq.push(['_trackEvent', 'Twitter', event.type])
+    }
+
     twttr.events.bind('click', track)
     twttr.events.bind('tweet', track)
     twttr.events.bind('follow', track)
   })
 
-  // Prism extensions
+  /**
+   * As Twitter forbids the use of the count API we need to rely on topsy for
+   * both the .com and the .io URL of this page.
+   */
+
+  async('http://otter.topsy.com/stats.js?callback=tweetCount&url=http://radlikewhoa.github.io/Countable/&apikey=ULDPO6D4UZ7FT2ZACMLQAAAAACA3WEW5ZNIQAAAAAAAFQGYA')
+  async('http://otter.topsy.com/stats.js?callback=tweetCount&url=http://radlikewhoa.github.com/Countable/&apikey=ULDPO6D4UZ7FT2ZACMLQAAAAACA3WEW5ZNIQAAAAAAAFQGYA')
+
+  window.callbacksCalled = []
+  window.tweetsSent = 0
+
+  window.tweetCount = function (data) {
+    if (!data) return
+
+    var counter = document.getElementById('tweet-count'),
+        tweets = data.response.all
+
+    if (!tweets) return
+
+    tweetsSent += tweets
+    callbacksCalled.push(tweets)
+
+    if (callbacksCalled.length == 2) {
+      if (tweetsSent > 0) {
+        text(counter, tweetsSent)
+        counter.parentNode.style.display = 'inline-block'
+        counter.parentNode.title = 'There are ' + tweetsSent + ' Tweets about Countable'
+      }
+
+      try {
+        delete window.tweetCount
+        delete window.tweetsSent
+        delete window.callbacksCalled
+      } catch (e) {
+        window.tweetCount = window.tweetsSent = window.callbacksCalled = undefined
+      }
+    }
+  }
+
+  /**
+   * Prism is used on the site to do syntax highlighting. In order to make the
+   * documentation more appealing, Countable-specific keywords are highlighted
+   * as well and can be styled using the given class.
+   *
+   * @see <http://goo.gl/6DpY8>
+   */
 
   Prism.languages.insertBefore('javascript', 'keyword', {
     'countable-class': /Countable/
   })
-
-  // Track clicks on the download link
-
-  if (window.addEventListener) {
-    githubButton.addEventListener('click', function () {
-      _gaq.push(['_trackEvent', 'Site', 'Downloads'])
-    })
-  } else if (window.attachEvent) {
-    githubButton.attachEvent('click', function () {
-      _gaq.push(['_trackEvent', 'Site', 'Downloads'])
-    })
-  }
 }(document))
