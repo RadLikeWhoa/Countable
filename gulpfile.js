a8e9b7d43d833311ab8c63c19fd8a96cd5a3d967
Sacha Schmid <schmid.sacha@gmail.com>
2014-08-16 22:43:17 +0200

    LiveReload HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ gulp.task('webserver', function () {
 gulp.task('watch', function () {
   gulp.watch('src/scss/**/*.scss', ['styles'])
   gulp.watch('src/js/**/*.js', ['jshint', 'scripts'])
+  gulp.watch('src/**/*.html', notifyLivereload)
   gulp.watch('dist/css/**/*.css', notifyLivereload)
   gulp.watch('dist/js/**/*.js', notifyLivereload)
 })
