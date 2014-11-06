ec5c495d35d06c617e7f5fe624c64eb6846c80b0
Sacha Schmid <schmid.sacha@gmail.com>
2014-11-06 10:33:33 +0100

    Update decode function

--- Countable.js
+++ Countable.js
@@ -72,19 +72,18 @@
     while (counter < length) {
       value = string.charCodeAt(counter++)
 
-      if ((value & 0xF800) == 0xD800 && counter < length) {
-
+      if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
         // High surrogate, and there is a next character.
-
         extra = string.charCodeAt(counter++)
 
         if ((extra & 0xFC00) == 0xDC00) {
-
           // Low surrogate.
-
           output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000)
         } else {
+          // unmatched surrogate; only append this code unit, in case the next
+          // code unit is the high surrogate of a surrogate pair
           output.push(value, extra)
+          counter--
         }
       } else {
         output.push(value)
