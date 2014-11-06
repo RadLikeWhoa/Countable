29a3b35e23cf8322045897fe917f8e78d67f52db
Sacha Schmid <schmid.sacha@gmail.com>
2014-11-06 10:19:59 +0100

    Update test script

--- test/test.js
+++ test/test.js
@@ -1,22 +1,21 @@
 var expect = chai.expect
 
 describe('Countable', function () {
-  var results = {},
-      liveArea, onceArea, optionsArea
+  var paragraphs, words, characters, all,
+      areaContainer, area
 
   function callback (counter) {
-    results.paragraphs.innerHTML = counter.paragraphs
-    results.words.innerHTML = counter.words
-    results.characters.innerHTML = counter.characters
-    results.all.innerHTML = counter.all
+    paragraphs.innerHTML = counter.paragraphs
+    words.innerHTML = counter.words
+    characters.innerHTML = counter.characters
+    all.innerHTML = counter.all
   }
 
-  function triggerInput (element) {
-    var event
-    event = document.createEvent('HTMLEvents')
+  function triggerInput () {
+    var event = document.createEvent('HTMLEvents')
     event.initEvent('input', true, true)
     event.eventName = 'input'
-    element.dispatchEvent(event)
+    area.dispatchEvent(event)
   }
 
   before(function () {
@@ -26,113 +25,122 @@ describe('Countable', function () {
                                 '<li id="characters"></li>' +
                                 '<li id="all"></li>' +
                               '</ul>' +
-                              '<textarea id="live"></textarea>' +
-                              '<textarea id="once"></textarea>' +
-                              '<textarea id="options"></textarea>'
+                              '<textarea id="area"></textarea>'
 
-    results.paragraphs = document.getElementById('paragraphs')
-    results.words = document.getElementById('words')
-    results.characters = document.getElementById('characters')
-    results.all = document.getElementById('all')
+    paragraphs = document.getElementById('paragraphs')
+    words = document.getElementById('words')
+    characters = document.getElementById('characters')
+    all = document.getElementById('all')
 
-    liveArea = document.getElementById('live')
-    onceArea = document.getElementById('once')
-    optionsArea = document.getElementById('options')
+    area = document.getElementById('area')
+  })
+
+  beforeEach(function () {
+    area.value = ''
   })
 
   describe('Countable.live', function () {
     it('should enable live counting', function () {
-      Countable.live(liveArea, callback)
-      expect(Countable.enabled(liveArea)).to.equal(true)
+      Countable.live(area, callback)
+      expect(Countable.enabled(area)).to.equal(true)
     })
 
     it('should count initially', function () {
-      expect(results.paragraphs.innerHTML).to.equal('0')
-      expect(results.words.innerHTML).to.equal('0')
-      expect(results.characters.innerHTML).to.equal('0')
-      expect(results.all.innerHTML).to.equal('0')
+      expect(paragraphs.innerHTML).to.equal('0')
+      expect(words.innerHTML).to.equal('0')
+      expect(characters.innerHTML).to.equal('0')
+      expect(all.innerHTML).to.equal('0')
     })
 
     it('should update counts', function () {
-      liveArea.value += 'Hello world'
+      area.value = 'Hello world'
 
-      triggerInput(liveArea)
+      triggerInput()
 
-      expect(results.paragraphs.innerHTML).to.equal('1')
-      expect(results.words.innerHTML).to.equal('2')
-      expect(results.characters.innerHTML).to.equal('10')
-      expect(results.all.innerHTML).to.equal('11')
+      expect(paragraphs.innerHTML).to.equal('1')
+      expect(words.innerHTML).to.equal('2')
+      expect(characters.innerHTML).to.equal('10')
+      expect(all.innerHTML).to.equal('11')
+    })
+
+    it('should kill live counting', function () {
+      Countable.die(area)
+      expect(Countable.enabled(area)).to.equal(false)
     })
   })
 
   describe('Countable.once', function () {
     it('should not enable live counting', function () {
-      Countable.once(onceArea, callback)
-      expect(Countable.enabled(onceArea)).to.equal(false)
+      Countable.once(area, callback)
+      expect(Countable.enabled(area)).to.equal(false)
     })
 
     it('should count initially', function () {
-      expect(results.paragraphs.innerHTML).to.equal('0')
-      expect(results.words.innerHTML).to.equal('0')
-      expect(results.characters.innerHTML).to.equal('0')
-      expect(results.all.innerHTML).to.equal('0')
+      expect(paragraphs.innerHTML).to.equal('0')
+      expect(words.innerHTML).to.equal('0')
+      expect(characters.innerHTML).to.equal('0')
+      expect(all.innerHTML).to.equal('0')
     })
 
     it('should not update counts', function () {
-      onceArea.value += 'Hello world'
+      area.value = 'Hello world'
 
-      triggerInput(onceArea)
+      triggerInput()
 
-      expect(results.paragraphs.innerHTML).to.equal('0')
-      expect(results.words.innerHTML).to.equal('0')
-      expect(results.characters.innerHTML).to.equal('0')
-      expect(results.all.innerHTML).to.equal('0')
+      expect(paragraphs.innerHTML).to.equal('0')
+      expect(words.innerHTML).to.equal('0')
+      expect(characters.innerHTML).to.equal('0')
+      expect(all.innerHTML).to.equal('0')
     })
-  })
 
-  describe('Countable.die', function () {
-    it('should kill live counting functionality', function () {
-      Countable.die(liveArea)
-      expect(Countable.enabled(liveArea)).to.equal(false)
+    it('should be aliased as count', function () {
+      area.value = 'Hello world'
+
+      Countable.count(area, callback)
+
+      expect(paragraphs.innerHTML).to.equal('1')
+      expect(words.innerHTML).to.equal('2')
+      expect(characters.innerHTML).to.equal('10')
+      expect(all.innerHTML).to.equal('11')
     })
   })
 
   describe('Options', function () {
-    afterEach(function () {
-      optionsArea.value = ''
+    beforeEach(function () {
+      area.value = ''
     })
 
     it('should strip HTML tags', function () {
-      optionsArea.value = '<div>Hello <a href="http://google.com">world</a></div>'
+      area.value = '<div>Hello <a href="http://google.com">world</a></div>'
 
-      Countable.once(optionsArea, callback, { stripTags: true })
+      Countable.once(area, callback, { stripTags: true })
 
-      expect(results.paragraphs.innerHTML).to.equal('1')
-      expect(results.words.innerHTML).to.equal('2')
-      expect(results.characters.innerHTML).to.equal('10')
-      expect(results.all.innerHTML).to.equal('11')
+      expect(paragraphs.innerHTML).to.equal('1')
+      expect(words.innerHTML).to.equal('2')
+      expect(characters.innerHTML).to.equal('10')
+      expect(all.innerHTML).to.equal('11')
     })
 
     it('should use hard returns', function () {
-      optionsArea.value = 'Hello\n\nworld'
+      area.value = 'Hello\n\nworld'
 
-      Countable.once(optionsArea, callback, { hardReturns: true })
+      Countable.once(area, callback, { hardReturns: true })
 
-      expect(results.paragraphs.innerHTML).to.equal('2')
-      expect(results.words.innerHTML).to.equal('2')
-      expect(results.characters.innerHTML).to.equal('10')
-      expect(results.all.innerHTML).to.equal('12')
+      expect(paragraphs.innerHTML).to.equal('2')
+      expect(words.innerHTML).to.equal('2')
+      expect(characters.innerHTML).to.equal('10')
+      expect(all.innerHTML).to.equal('12')
     })
 
     it('should ignore returns', function () {
-      optionsArea.value = 'Hello\nworld'
+      area.value = 'Hello\nworld'
 
-      Countable.once(optionsArea, callback, { ignoreReturns: true })
+      Countable.once(area, callback, { ignoreReturns: true })
 
-      expect(results.paragraphs.innerHTML).to.equal('2')
-      expect(results.words.innerHTML).to.equal('2')
-      expect(results.characters.innerHTML).to.equal('10')
-      expect(results.all.innerHTML).to.equal('10')
+      expect(paragraphs.innerHTML).to.equal('2')
+      expect(words.innerHTML).to.equal('2')
+      expect(characters.innerHTML).to.equal('10')
+      expect(all.innerHTML).to.equal('10')
     })
   })
 })
