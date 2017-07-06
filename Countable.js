7a15a01ff1823042489e02d7fcb73378f3901d25
Sacha Schmid <s.schmid@schwabe.ch>
2017-07-06 12:33:05 +0200

    Bump version

--- Countable.js
+++ Countable.js
@@ -3,7 +3,7 @@
  * counting on an HTML element.
  *
  * @author   Sacha Schmid (<https://github.com/RadLikeWhoa>)
- * @version  2.1.1
+ * @version  3.0.0
  * @license  MIT
  * @see      <http://radlikewhoa.github.io/Countable/>
  */
